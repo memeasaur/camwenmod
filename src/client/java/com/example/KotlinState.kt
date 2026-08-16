93dda64268ed0859f096929eef55d4820a7b39f6
cuckm <cuckmeron@gmail.com>
2026-08-16 01:20:21 -0400

    .

--- src/client/java/com/example/KotlinState.kt
+++ src/client/java/com/example/KotlinState.kt
@@ -2,12 +2,20 @@ package com.example
 
 import io.github.jan.supabase.SupabaseClient
 import io.github.jan.supabase.auth.Auth
+import io.github.jan.supabase.auth.auth
 import io.github.jan.supabase.createSupabaseClient
 import io.github.jan.supabase.postgrest.Postgrest
 import io.github.jan.supabase.postgrest.from
+import io.github.jan.supabase.realtime.PostgresAction
 import io.github.jan.supabase.realtime.Realtime
 import io.github.jan.supabase.realtime.channel
+import io.github.jan.supabase.realtime.postgresChangeFlow
 import io.github.jan.supabase.realtime.realtime
+import kotlinx.coroutines.flow.subscribe
+import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import net.minecraft.item.ItemStack
 
 val supabaseClient: SupabaseClient = createSupabaseClient(
     supabaseUrl = "https://kshvmmimjsffefrfnuxl.supabase.co",
@@ -16,10 +24,52 @@ val supabaseClient: SupabaseClient = createSupabaseClient(
     install(Auth)
     install(Postgrest)
     install(Realtime)
-}
+}.auth.signInWith()
+
+class VisiblePlayer(
+    // TODO -> data?
+    val tableEntry: VisiblePlayerTableEntry,
+    val nullableInventorySlots: Array<ItemStack>?, // TODO -> 36
+)
+
+@Serializable
+data class VisiblePlayerTableEntry(
+    val uuid: String,
+    @SerialName("location_x")
+    val locationX: Float,
+    @SerialName("location_y")
+    val locationY: Float,
+    @SerialName("location_z")
+    val locationZ: Float,
+    @SerialName("nullable_health")
+    val health: Float?,
+    @SerialName("team_color")
+    val teamColor: String,
+    @SerialName("health_consumables_used")
+    val healthConsumablesUsed: Int
+)
+
+@Serializable
+data class AccountInventorySlotsEntry(
+    val uuid: String,
+    val index: Int,
+)
+
 TODO; // -> subscribe -> select * -> apply all buffered events -> synchronized
-val players = supabaseClient
-    .from("players")
-    .select("*")
-    .decodeList<>()
-val channel = supabaseClient.realtime.channel("players")
+val channel = runBlocking {
+    TODO; // block these from mutating until the collection is ready
+    val channel = supabaseClient.realtime.channel("party_visible_accounts")
+    channel.postgresChangeFlow<PostgresAction>("public") {
+        table = "party_visible_accounts"
+    }.collect { new ->
+        TODO; // apply
+    }
+    channel.subscribe()
+
+}
+val players = runBlocking { // TODO -> async
+    supabaseClient
+        .from("party_visible_accounts")
+        .select()
+        .decodeList<VisiblePlayerTableEntry>()
+}
