ae548d8f44a6e4b8224b7d8f5f166f9a607e9b3f
cuckm <cuckmeron@gmail.com>
2026-08-19 03:58:29 -0400

    .

---
+++ src/client/java/com/example/mixins/ClientWorldMixin.java
@@ -0,0 +1,19 @@
+package com.example.mixins;
+
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ClientWorld.class)
+public class ClientWorldMixin {
+    @Inject(method = "addEntity", at = @At("HEAD"))
+    void onAddEntity(Entity entity, CallbackInfo ci) {
+        if (entity instanceof PlayerEntity player) {
+            TODO;
+        }
+    }
+}
