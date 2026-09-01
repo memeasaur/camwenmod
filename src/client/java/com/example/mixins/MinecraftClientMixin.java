5066df568a4650ba22e81d9339b1823067ca73b5
cameron <cuckmeron@gmail.com>
2026-09-01 05:46:11 -0400

    aaaaa

--- src/client/java/com/example/mixins/MinecraftClientMixin.java
+++ src/client/java/com/example/mixins/MinecraftClientMixin.java
@@ -64,62 +64,11 @@ public abstract class MinecraftClientMixin {
                     Text.literal("miss penalty: " + previousAttackCooldown + " -> " + MINECRAFT_CLIENT_INSTANCE.attackCooldown),
                     false);
         }
-
-//        float attackCooldown = player.getAttackCooldownProgress(0f);
-//        boolean isWeakAttack = player.getAttackCooldownProgress(.5f) < KNOCKBACK_ATTACK_STRENGTH;
-//        lastHitStrength = attackCooldown < 1f
-//                ? Text.literal(String.format("%.2f", attackCooldown))
-//                .setStyle(isWeakAttack
-//                        ? RED_TEXT_STYLE
-//                        : GREEN_TEXT_STYLE)
-//                : EMPTY_TEXT;
-//        if (isWeakAttack) {
-////            if (config.isAttackCooldownWarningEnabled) {
-////                player.playSound(SoundEvents.BLOCK_ANVIL_LAND, .25f, .25f); // TODO this should just replace the weak attack sound (?) -> the sounds for miss etc. should just be entirely configurable
-////            }
-//        }
-//        lastHitDisplayTimer = 0;
         if (MINECRAFT_CLIENT_INSTANCE.crosshairTarget instanceof EntityHitResult entityHitResult && entityHitResult.getEntity() instanceof LivingEntity target) {
-//            lastHitDistance = String.format("%.2f", player.getEyePos().distanceTo(entityHitResult.getPos()));
-
-//            if (!isWeakAttack) {
-//                if (isSprintReset && player.isSprinting()) {
-////                    if (config.isKnockbackParticleEnabled)
-////                        MINECRAFT_CLIENT_INSTANCE.particleManager.addEmitter(target, new BlockStateParticleEffect(ParticleTypes.BLOCK, Blocks.COBWEB.getDefaultState()));
-////                    // TODO ParticleTypes.FLASH, ParticleTypes.FIREWORK, ParticleTypes.ELECTRIC_SPARK, ParticleTypes.COMPOSTER, ParticleTypes.GLOW, ParticleTypes.HAPPY_VILLAGER, ParticleTypes.HEART, ParticleTypes.SCRAPE, ParticleTypes.WAX_OFF, ParticleTypes.WAX_ON, or just list all particles in the options dropdown, that's better
-////                    isSprintReset = false;
-//                } else if (!isYLower  // player.fallDistance <= 0.0F
-//                        || player.isOnGround()
-//                        || player.isClimbing()
-//                        || player.isTouchingWater()
-//                        || player.hasStatusEffect(StatusEffects.BLINDNESS)
-//                        || player.hasVehicle()
-////                                && target instanceof LivingEntity
-////                            && !player.isSprinting()
-//                ) { // this is pulled from: playerEntity.java -> attack()
-//                    if (config.isSweepParticleEnabled)
-//                        MINECRAFT_CLIENT_INSTANCE.particleManager.addEmitter(target, new BlockStateParticleEffect(ParticleTypes.BLOCK, Blocks.BEACON.getDefaultState()));
-//                    if (config.isSweepAttackWarningEnabled)
-//                        player.playSound(SoundEvents.BLOCK_ANVIL_LAND, .25f, .25f);
-//                    // TODO -> selector for all sounds to pick for this
-//                }
-//            }
-
-//            if (config.isBleedParticleEnabled) {
-//                MINECRAFT_CLIENT_INSTANCE.particleManager.addEmitter(target, new BlockStateParticleEffect(ParticleTypes.BLOCK, Blocks.REDSTONE_WIRE.getDefaultState().with(Properties.POWER, 1))); // TODO Blocks.RED_CANDLE.getDefaultState()
-//            }
-
             if (entityHitResult.getEntity() instanceof PlayerEntity) {
                 onPvpDamage();
             }
-
-//            if (config.isSharpnessParticleReverted && player.getMainHandStack().getEnchantments().getEnchantmentEntries().stream().anyMatch(x -> Enchantments.SHARPNESS.equals(x.getKey().getKey().orElse(null)))) // TODO fuck
-//                player.addEnchantedHitParticles(target);
-//            if (config.isCritParticleReverted && player.getMovement().getY() < 0.0f && !player.isOnGround() && !player.isClimbing() && !player.isTouchingWater() && !player.hasStatusEffect(StatusEffects.BLINDNESS) && !player.hasVehicle()) // TODO -> p.isSprinting() config option + player.fallDistance > 0.0F?
-//                player.addCritParticles(target);
         }
-//        else
-//            lastHitDistance = "";
     }
 
     @Inject(at = @At(value = "RETURN"), method = "doAttack")
@@ -157,8 +106,4 @@ public abstract class MinecraftClientMixin {
             }
         }
     }
-//    @Inject(method = "doItemUse", at = @At(value = "HEAD"))
-//    private void onDoItemUse(CallbackInfo ci) {
-//        MINECRAFT_CLIENT_INSTANCE.interactionManager.isBreakingBlock()
-//    } TODO REMOVE
 }
