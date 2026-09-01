7ec13efbdbb778518031b0df975397f0811db673
cameron <cuckmeron@gmail.com>
2026-09-01 05:34:46 -0400

    aaaaa

--- src/client/java/com/example/Configs/Config.java
+++ src/client/java/com/example/Configs/Config.java
@@ -1,14 +1,11 @@
 package com.example.Configs;
 
-import org.lwjgl.glfw.GLFW;
-
 import java.util.Map;
 import java.util.UUID;
 
 import static com.example.Utils.serializeJsonBlocking;
 
 public class Config {
-    // TODO -> serialize configs in server (?)
     public Map<UUID, String> nameplateUuids = Map.of();
     public boolean isToggleSneakGuiEnabled = false;
     public boolean isSneakEnabled = false;
@@ -16,16 +13,8 @@ public class Config {
     public boolean isFullbrightEnabled = false;
     public boolean isFlyBoostEnabled = false;
     public String currentPotionEnchantmentGlintType = "";
-//    public boolean isAttackLoweringDisabled = false;
 //    public boolean isSharpnessParticleReverted = false;
 //    public boolean isCritParticleReverted = false;
-//    public boolean isKnockbackParticleEnabled = false;
-//    public boolean isSweepParticleEnabled = false;
-//    public boolean isBleedParticleEnabled = false;
-//    public boolean isAttackCooldownNotificationEnabled = false;
-//    public boolean isAttackCooldownWarningEnabled = false;
-//    public boolean isSweepAttackWarningEnabled = false;
-//    public boolean isAttackIndicatorDataEnabled = false;
     public boolean isWeakAttackSoundDisabled = false;
     public boolean isDamageTakenValueNotificationEnabled = false;
     public boolean isDepthStriderReverted = true;
@@ -33,15 +22,9 @@ public class Config {
 
     public boolean isMovementTogglePvpDisabling = false;
     public boolean isMovementToggleMirrorPressDisabling = false;
-
-    //    TODO; // put all keybinds in the real keybind thing?
     public boolean isGuiCheatsPvpDisabling = false;
-    public int glfwToggleBlockXrayKeybind = GLFW.GLFW_KEY_UNKNOWN; // TODO -> hold/active/disable
-    public int glfwTogglePlayerXrayKeybind = GLFW.GLFW_KEY_UNKNOWN;
-
     public boolean isAutoCobweb = false;
     public boolean isDarknessDisabled = false;
-
     // TODO -> should have an option for it making a noise if a player shows up nearby
     public boolean isPlayerWaypointsEnabled = false;
 
