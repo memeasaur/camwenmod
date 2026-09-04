66633620a330e89e812b74fc3549cd520bf1c6fb
cuckm <cuckmeron@gmail.com>
2026-09-04 01:10:20 -0400

    .

--- src/client/java/com/example/mixins/SodiumBlockOcclusionCacheMixin.java
+++ src/client/java/com/example/mixins/SodiumBlockOcclusionCacheMixin.java
@@ -1,7 +1,6 @@
 package com.example.mixins;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
-import net.caffeinemc.mods.sodium.client.render.chunk.compile.pipeline.BlockOcclusionCache;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.BlockGetter;
@@ -13,13 +12,14 @@ import java.util.function.BiFunction;
 
 import static com.example.UntitledClient.nullableImmutableState;
 
-@Mixin(BlockOcclusionCache.class)
-public class SodiumBlockOcclusionCacheMixin {
-    @ModifyReturnValue(method = "shouldDrawSide", at = @At("RETURN"))
-    private boolean shouldDrawSide(boolean original, BlockState state, BlockGetter view, BlockPos pos, Direction facing) {
-        if (nullableImmutableState != null)
-            return ((BiFunction<BlockState, Boolean, Boolean>) nullableImmutableState.get("SHOULD_DRAW_SIDE_MIXIN")).apply(state, original);
-        else
-            return original;
-    }
-}
+// TODO -> update
+//@Mixin(BlockOcclusionCache.class)
+//public class SodiumBlockOcclusionCacheMixin {
+//    @ModifyReturnValue(method = "shouldDrawSide", at = @At("RETURN"))
+//    private boolean shouldDrawSide(boolean original, BlockState state, BlockGetter view, BlockPos pos, Direction facing) {
+//        if (nullableImmutableState != null)
+//            return ((BiFunction<BlockState, Boolean, Boolean>) nullableImmutableState.get("SHOULD_DRAW_SIDE_MIXIN")).apply(state, original);
+//        else
+//            return original;
+//    }
+//}
