252f9088f8d973032cccb1192de1a924c6f6bfd5
cuckm <cuckmeron@gmail.com>
2026-09-18 01:56:13 -0400

    .

--- src/client/java/com/example/mixins/CameraMixin.java
+++ src/client/java/com/example/mixins/CameraMixin.java
@@ -11,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import static com.example.Constants.JUMP_VANILLA;
+import static com.example.DelayedConstantsTodo.JUMP_VANILLA;
 import static com.example.UntitledClient.headRunCameraOffset;
 
 @Mixin(Camera.class)
