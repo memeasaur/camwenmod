029554b941514d5fece58356d4526aae560d52ef
cuckm <cuckmeron@gmail.com>
2026-09-23 03:09:05 -0400

    .

--- src/client/java/com/example/Configs/CheatConfig.java
+++ src/client/java/com/example/Configs/CheatConfig.java
@@ -5,6 +5,8 @@ public class CheatConfig {
     public boolean isTargetingMarginReverted = false; // TODO -> struct
     public float staticTargetingMarginBypass = .0f;
     public float movingTargetMarginBypass = 0.f;
+    TODO;
+    public float doubleMovingTargetMarginBypass = 0.f;
 //    public float targetingMarginWidthBypass = 0.f;
 //    public double attackVelocityBypass = 0.6;
 //    public boolean isAutoCobweb = false; // TODO -> struct?
