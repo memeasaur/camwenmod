ca564a813da802c99c04a715b83186a2f7812798
cuckm <cuckmeron@gmail.com>
2026-09-25 04:00:40 -0400

    .

--- src/client/java/com/example/mixins/AbstractClientPlayerMixinTodoAi.java
+++ src/client/java/com/example/mixins/AbstractClientPlayerMixinTodoAi.java
@@ -20,35 +20,36 @@ import static com.example.Utils.computeCheatConfig;
 
 @Mixin(AbstractClientPlayer.class)
 public abstract class AbstractClientPlayerMixinTodoAi {
-    @Unique
-    private static final Identifier SPRINTING_MODIFIER_ID = Identifier.withDefaultNamespace("sprinting");
-
-    @Redirect(
-            method = "getFieldOfViewModifier",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/client/player/AbstractClientPlayer;getAttributeValue(Lnet/minecraft/core/Holder;)D"))
-    private double getEthyleneFovMovementSpeed(
-            AbstractClientPlayer player, Holder<Attribute> attribute) {
-        double movementSpeed = player.getAttributeValue(attribute);
-        CheatConfig cheatConfig = computeCheatConfig();
-        if (!config.isCheatsEnabled ||
-                !cheatConfig.isEthylene ||
-                player != MINECRAFT_CLIENT_INSTANCE.player ||
-                !player.isSprinting() ||
-                attribute != Attributes.MOVEMENT_SPEED) {
-            isEthyleneSprintFovCancelled = false;
-            return movementSpeed;
-        }
-
-        if (!isEthyleneSprintFovCancelled) {
-            return movementSpeed;
-        }
-
-        AttributeInstance movementSpeedAttribute = player.getAttribute(Attributes.MOVEMENT_SPEED);
-        AttributeModifier sprintingModifier = movementSpeedAttribute.getModifier(SPRINTING_MODIFIER_ID);
-        return sprintingModifier == null
-                ? movementSpeed
-                : movementSpeed / (1.0D + sprintingModifier.amount());
-    }
+    // TODO gl
+//    @Unique
+//    private static final Identifier SPRINTING_MODIFIER_ID = Identifier.withDefaultNamespace("sprinting");
+//
+//    @Redirect(
+//            method = "getFieldOfViewModifier",
+//            at = @At(
+//                    value = "INVOKE",
+//                    target = "Lnet/minecraft/client/player/AbstractClientPlayer;getAttributeValue(Lnet/minecraft/core/Holder;)D"))
+//    private double getEthyleneFovMovementSpeed(
+//            AbstractClientPlayer player, Holder<Attribute> attribute) {
+//        double movementSpeed = player.getAttributeValue(attribute);
+//        CheatConfig cheatConfig = computeCheatConfig();
+//        if (!config.isCheatsEnabled ||
+//                !cheatConfig.isEthylene ||
+//                player != MINECRAFT_CLIENT_INSTANCE.player ||
+//                !player.isSprinting() ||
+//                attribute != Attributes.MOVEMENT_SPEED) {
+//            isEthyleneSprintFovCancelled = false;
+//            return movementSpeed;
+//        }
+//
+//        if (!isEthyleneSprintFovCancelled) {
+//            return movementSpeed;
+//        }
+//
+//        AttributeInstance movementSpeedAttribute = player.getAttribute(Attributes.MOVEMENT_SPEED);
+//        AttributeModifier sprintingModifier = movementSpeedAttribute.getModifier(SPRINTING_MODIFIER_ID);
+//        return sprintingModifier == null
+//                ? movementSpeed
+//                : movementSpeed / (1.0D + sprintingModifier.amount());
+//    }
 }
